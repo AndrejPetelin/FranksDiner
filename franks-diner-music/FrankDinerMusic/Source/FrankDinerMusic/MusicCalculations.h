5d6cdf69f5907536734e961ff3622c37eeb1d8b8
Andrej Petelin <andrej.petelin.1@gmail.com>
2026-01-21 16:42:50 +0100

    feat: triing to find wire the musitian

---
+++ franks-diner-music/FrankDinerMusic/Source/FrankDinerMusic/MusicCalculations.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MusicCalculations.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRANKDINERMUSIC_API UMusicCalculations : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
